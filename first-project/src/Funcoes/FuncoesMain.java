32a12bd291a6110a8ab6a15009e353140e5f4ce3
Lucas Do Prado <lucas.ribeiro@sonner.com.br>
2024-03-06 13:08:16 -0300

    Criação do folder de funções e classe de função

---
+++ first-project/src/Funcoes/FuncoesMain.java
@@ -0,0 +1,32 @@
+package Funcoes;
+
+public class FuncoesMain {
+    /* Na programação precisamos resolver diversos problemas, sendo comum
+       precisar resolver uma mesma situação diversas vezes.
+
+       Exemplo: Imprimir um texto com as letras MAISCULAS
+
+       System.out.println("texto a ser convertido para maiusculo".toUpperCase());
+
+       Para resolver
+
+       1 - Usamos a função println() que irá imprimir o texto passado
+       2 - E utilizamos a função toUpperCase() para deixar em maisculo o texto
+
+        Em um primeiro momento vemos que o problema pode ser resolvido dessa forma
+
+        Imaginemos que vamos precisar utilizar essa mesma lógica em diversos lugares
+        Precisariamos repetir esse mesmo trecho todas as vezes, para escrever nosso texto
+        em maiusculo.
+
+        Mas será que não existe uma maneira mais facil de resolver esse problema sem repetir
+        esse código toda vez?
+
+    */
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- first-project/src/Main.java
+++ first-project/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         // Alteração da classe main
+
+
+        //Meu comentário é melhor
         System.out.println("Hello world!");
     }
 }
