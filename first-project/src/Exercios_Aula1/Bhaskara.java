efdfd5bec401d6dafb647b5bef67a178a7a9068c
VíctorHugo <regysvictor@gmail.com>
2024-03-07 13:34:05 -0300

    meu primeiro commit, criado a classe bhaskara

---
+++ first-project/src/Exercios_Aula1/Bhaskara.java
@@ -0,0 +1,20 @@
+package Exercios_Aula1;
+
+import java.sql.SQLOutput;
+
+public class Bhaskara {
+    public static void main(String[] args) {
+    System.out.println("Hello World");
+
+    }
+    public static void calcularBhaskara(int a, int b, int c){
+
+    }
+
+    public static int calcularDelta(int a, int b, int c){
+       return b * 2 - 4 * a * c;
+
+    }
+
+
+}
