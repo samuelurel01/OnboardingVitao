e086269dda42f346222457764088859f4ade4908
samuelurel <samuel.souza@sonner.com.br>
2024-03-08 14:12:04 -0300

    muitos exercicios

---
+++ first-project/src/Exercios_Aula1/Exemplos.java
@@ -0,0 +1,19 @@
+package Exercios_Aula1;
+
+public class Exemplos {
+
+    public static void main(String[] args) {
+
+
+        int[] teste = new int[]{};
+
+
+        teste[0] = 25;
+        teste[1] = 30;
+
+
+        //teste = [25, 30]
+
+
+    }
+}
